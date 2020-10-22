fe7bf4a92e989d76f29fb8203a59ab466426484d
jfalxa <jfalxa.sta@gmail.com>
2020-10-23 01:42:12 +0200

    fix app not running

--- lib/main.dart
+++ lib/main.dart
@@ -7,26 +7,27 @@ import 'src/routes/article_web_view.dart';
 import 'src/routes/subscription_feed.dart';
 
 void main() {
-  ChangeNotifierProvider(
+  runApp(ChangeNotifierProvider(
     create: (context) => Store(),
     child: RssFeed(),
-  );
+  ));
 }
 
 class RssFeed extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-        title: 'Rss Feed',
-        theme: ThemeData(
-          primarySwatch: Colors.blue,
-          visualDensity: VisualDensity.adaptivePlatformDensity,
-        ),
-        initialRoute: '/',
-        routes: {
-          '/': (context) => App(),
-          ArticleWebView.routeName: (context) => ArticleWebView(),
-          SubscriptionFeed.routeName: (context) => SubscriptionFeed()
-        });
+      title: 'Rss Feed',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      initialRoute: '/',
+      routes: {
+        '/': (context) => App(),
+        ArticleWebView.routeName: (context) => ArticleWebView(),
+        SubscriptionFeed.routeName: (context) => SubscriptionFeed()
+      },
+    );
   }
 }
